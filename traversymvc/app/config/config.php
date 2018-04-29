1b42da11aa2fa24ccc5f8493491f768fbb201566
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2018-04-29 09:00:47 +0200

    Adding config for database class

--- traversymvc/app/config/config.php
+++ traversymvc/app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+  //DB params get via env variables
+  define('DB_HOST' , getenv('MYSQL_HOST'));
+  define('DB_USER' , getenv('MYSQL_USER'));
+  define('DB_PASS' , getenv('MYSQL_ROOT_PASSWORD'));
+  define('DB_NAME' , 'tmvc');
+
   // App Root
   define('APPROOT' , dirname(dirname(__FILE__)));
   
---
+++ traversymvc/app/libraries/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+  /*
+  *  PDO Database class
+  *  Connect to database
+  *  Create prepared statements
+  *  Bind values
+  *  Returns rows and results
+  */ 
+
+  class Database {
+    private $host   = DB_HOST;
+    private $user   = DB_USER;
+    private $pass   = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct(){
+      $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname ;
+      $options = array(
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      );
+
+      // Create pdo instance
+
+      try{
+        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+      } catch(PDOException $e) {
+        $this-> error = $e->getMessage();
+        echo $this->error;
+      }
+
+
+    }
+
+
+  }
+
+?>  
